7ba4fac4e94282af440d3d08f2a62b08d184d412
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-04-13 17:08:29 -0500

    clearance levels

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const userStrategy = require('../strategies/user.strategy');
 
-router.get('/', (req, res) => {
+
+router.get('/', rejectUnauthenticated, (req, res) => {
   // what is the value of req.user????
   console.log('req.user:', req.user);
-
+  if(req.user.clearance_level> 13){
   pool
     .query(`SELECT * FROM "secret";`)
     .then((results) => res.send(results.rows))
@@ -13,6 +19,25 @@ router.get('/', (req, res) => {
       console.log('Error making SELECT for secrets:', error);
       res.sendStatus(500);
     });
+  }
+  if(req.user.clearance_level> 6){
+    pool
+      .query(`SELECT * FROM "secret" WHERE "secret".secrecy_level <7;`)
+      .then((results) => res.send(results.rows))
+      .catch((error) => {
+        console.log('Error making SELECT for secrets:', error);
+        res.sendStatus(500);
+      });
+    }
+    if(req.user.clearance_level> 3){
+      pool
+        .query(`SELECT * FROM "secret" WHERE "secret".secrecy_level <4;`)
+        .then((results) => res.send(results.rows))
+        .catch((error) => {
+          console.log('Error making SELECT for secrets:', error);
+          res.sendStatus(500);
+        });
+      }
 });
 
 module.exports = router;
